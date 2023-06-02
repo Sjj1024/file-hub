47c2dd0b7dfab519568c39d968a04c1a2557aeb6
songjj <songjj@zetyun.com>
2023-06-02 18:52:36 +0800

    add:导航

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-    <div class="index">
-      <div class="box">首页: {{ user.name }}</div>
-    </div>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header">Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
+
 
 <script setup lang="ts">
 import { useUserStore } from './stores/user';
@@ -15,9 +22,5 @@ console.log("用户信息：", user.name);
 </script>
 
 <style scoped lang="scss">
-.index{
-  .box{
-    color: red;
-  }
-}
+
 </style>
