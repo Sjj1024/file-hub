a1250c00b899978fda7abde363a5f55a9675581c
songjj <songjj@zetyun.com>
2023-07-12 19:15:14 +0800

    fix:chat

--- src/views/chatgpt/index.vue
+++ src/views/chatgpt/index.vue
@@ -1,12 +1,40 @@
 <template>
-  <div>
-    chatgpt聊天
+  <div class="chat-box">
+    <el-icon @click="drawer = true" class="guide-ico">
+      <Guide />
+    </el-icon>
+    <!-- 左侧聊天列表 -->
+    <el-drawer v-model="drawer" title="ChatGpt" direction="ltr" class="chat-list">
+      <span>Hi there!</span>
+    </el-drawer>
   </div>
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
-
+const drawer = ref(true)
 </script>
 
-<style scoped lang='scss'></style>
+<style scoped></style>
+
+<style lang='scss'>
+.chat-list {
+  width: 20% !important;
+}
+
+.chat-box {
+  position: relative;
+
+  .guide-ico {
+    position: absolute;
+    top: 20px;
+    left: 30px;
+    font-size: 20px;
+  }
+
+  .el-overlay {
+    position: unset !important;
+    height: unset;
+  }
+}
+</style>
