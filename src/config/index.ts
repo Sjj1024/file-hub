631d21c56142301dfb37cf8851049b243b20b38c
songjj <songjj@zetyun.com>
2023-07-18 15:40:25 +0800

    feat:安全

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,12 @@
 // Filehub根路径地址：用于存储文件和被Frok
-export const fileHubBoss = "https://api.github.com/repos/Sjj1024/FileHub/forks"
+export const fileHubBoss = ""
 // DataHub根路径：用于用户注册，分享内容，评论等
-export const bossUrl = "https://api.github.com/repos/Sjj1024/DataHub/issues"
-// 1024小神token：用于开发测试
-export const bossToken = 'ghp_888grzs67MqxbZUH3wmIFKzecaKB0cTLy3ICBkl'.replace('888', '')
-// 1024回家token：用于开发测试
-export const guestToken = 'ghp_888LSkJC7DbB8pgMw6mynhQGLienoPv4P0pOLZ0'.replace('888', '')
+export const bossUrl = ""
+// 小神token：用于开发测试
+export const bossToken = ""
+// 家token：用于开发测试
+export const guestToken = ""
+
+// 公私钥加解密
+export const publickKey = ``
+export const privateKey = ``
